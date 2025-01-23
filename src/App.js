7f0921f148cfcc3dfbaa2d6e23d70a61706d52b0
Ross Alexander <ross.alexander7@gmail.com>
2025-01-23 00:24:58 -0800

    Add OpenWeatherMap API call functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
-function App() {
+// Displays current info
+function CurrentWeather({ data }) {
+  {/* Display weather data here */ }
+  return (
+    <div>{data ?
+      (
+        <div className='weather-container'>
+          <p className='temperature'>Temperature: </p>
+          <p className='description'>Condition: </p>
+        </div>
+      ) : (
+        <p>Loading weather data...</p>
+      )}</div>);
+}
+
+// // Search bar
+// function SearchBar({ onSearch }) {
+
+// }
+
+function WeatherApp() {
+
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState('San Francisco');
+  const apiKey = process.env.REACT_APP_OPEN_WEATHER_MAP_API_KEY;
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+      );
+      const data = await response.json();
+      setWeatherData(data);
+    };
+
+    fetchWeatherData();
+  }, [city])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <SearchBar onSearch={setCity} /> */}
+      <h2 className='city-name'>{city}</h2>
+      <CurrentWeather data={weatherData} />
     </div>
   );
 }
 
-export default App;
+export default WeatherApp;
