9baef96fabb3f44401428e7d07c51accf221f5a9
Ross Alexander <ross.alexander7@gmail.com>
2025-01-27 23:41:44 -0800

    Add dynamic background color

--- src/App.js
+++ src/App.js
@@ -43,8 +43,45 @@ function WeatherApp() {
     // Update if any of the following change.
     [query, location, units, lang, apiKey]);
 
+  /**
+   * Generates a background gradient color based on the time of day
+   * at the given timezone.
+   * 
+   * @param {Object} params - Object containing the timezone to use.
+   * @param {number} params.timezone - The timezone offset in seconds.
+   * 
+   * @returns {string} A linear gradient background string in CSS format.
+   * 
+   * The gradient changes based on the time of day at the given timezone.
+   * The colors are:
+   * - Nighttime (9pm - 6am): #4E598C to #2F3542
+   * - Sunrise (6am - 9am): #FFC107 to #4E598C
+   * - Daytime (9am - 6pm): #F4F4FA to #1E90FF
+   * - Sunset (6pm - 9pm): #FFC107 to #4E598C
+   */
+  function backgroundColor({ timezone }) {
+
+    // Get the current local hour in 24-hour format
+    const hour = new Date(Date.now() + (weatherData?.timezone * 1000)).getUTCHours();
+
+    // TODO: Refactor this function to use actual sunrise and sunset times
+    // Nighttime gradient
+    if (hour >= 21 || hour < 6) {
+      return `linear-gradient(to bottom, #4E598C, #2F3542)`;
+      // Sunrise gradient
+    } else if (hour >= 6 && hour < 9) {
+      return `linear-gradient(to bottom, #FFC107, #4E598C)`;
+      // Daytime gradient
+    } else if (hour >= 9 && hour < 18) {
+      return `linear-gradient(to bottom,rgb(228, 228, 250),rgb(30, 149, 246))`;
+      // Sunset gradient
+    } else if (hour >= 18 && hour < 21) {
+      return `linear-gradient(to bottom, #FFC107, #4E598C)`;
+    }
+  }
+
   return (
-    <div className="App">
+    <div className="App" style={{ background: backgroundColor({ timezone: weatherData?.timezone }) }}>
       <div className='header'>
         <div className='logo-container'>
           <img src={logo} className='logo' alt='logo' />
